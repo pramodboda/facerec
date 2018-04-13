f8ab7264a47e47c80367575bb7443fcc3c4add24
surya <surya.phani1729@gmail.com>
2018-04-13 19:30:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 
+function hasGetUserMedia() {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+}
+
+if (hasGetUserMedia()) {
+  alert('good to go');
+} else {
+  alert('getUserMedia() is not supported by your browser');
+}
