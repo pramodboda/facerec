d61639622a4e71843b65789d7bfc352ebfd1c961
surya <surya.phani1729@gmail.com>
2018-04-13 18:53:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 
+var context;
+window.addEventListener('load', init, false);
+function init() {
+  try {
+    alert("This browser support Web Audio API");
+    window.AudioContext = window.AudioContext||window.webkitAudioContext;
+    context = new AudioContext();
+  }
+  catch(e) {
+    alert('Web Audio API is not supported in this browser');
+  }
+}
