22bbee5e6f1e8f39e9d1806c6783405525b91667
surya <surya.phani1729@gmail.com>
2018-04-13 19:32:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,32 @@
 function hasGetUserMedia() {
   return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
 }
+const constraints = {
+  video: true
+};
+
+const video = document.querySelector('video');
+function handleSuccess(stream) {
+  video.srcObject = stream;
+}
+
+function handleError(error) {
+  console.error('Reeeejected!', error);
+}
+
 
 if (hasGetUserMedia()) {
-  alert('good to go');
+
+  
+  
+
+
+navigator.mediaDevices.getUserMedia(constraints).
+  then(handleSuccess).catch(handleError);
+  
+  
+  
+  
 } else {
   alert('getUserMedia() is not supported by your browser');
 }
